5cef6d9174ceb8833b90504558c1f033d5e32f23
martins160 <martins160@gmail.com>
2016-12-20 08:51:19 -0200

    hoje

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
 var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
+var watch = require('gulp-watch');
 
 //task sass
 gulp.task('sass', function(){
-	return sass('sass/*.sass').pipe(gulp.dest('css'))
+	return sass('sass/**/*.sass').pipe(gulp.dest('css'));
+});
+
+//task watch
+gulp.task('watch', function(){
+	gulp.watch('sass/**/*.sass', ['sass']);
 });
 
 //task default gulp
-gulp.task('default', function(){
-
-});
+gulp.task('default', ['sass', 'watch']);
